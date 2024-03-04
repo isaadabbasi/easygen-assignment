9e5039eaecd4d1b28b2d3accf9cb627f0bf7f307
isaadabbasi <itssaadabbasi@gmail.com>
2024-03-04 18:05:28 +0500

    chore: add shadcn

--- frontend/src/pages/home/home.page.tsx
+++ frontend/src/pages/home/home.page.tsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { Button } from "src/components/ui/button";
+import { Slider } from "src/components/ui/slider";
 import { authService, persistantStorageService } from "src/services";
 import { constants } from "src/utils";
 import { IAuth } from "src/defs";
 
 import "./home.page.css";
 
+ 
 const { AppRoutes } = constants;
 
 export function HomePage() {
   const navigateTo = useNavigate();
 
+  const [pbjSize, setPBJSize] = useState<number[]>([50])
   const [user, setUser] = useState<IAuth.IUser>({
     _id: "",
     createdAt: "",
@@ -30,28 +34,40 @@ export function HomePage() {
     navigateTo(AppRoutes.SignIn);
   };
 
+  const onSliderChange = (values: number[]) => {
+    setPBJSize(values)
+  }
+
   return (
-    <div id="home-page">
+    <div id="home-page" className='flex'>
       <div className="my-0 mx-auto text-center">
         <div className="hero">
-          <h2>Hello! {user.name}</h2>
+          <p className='h-2 font-bold uppercase'>Hello! {user.name}</p>
+          <br />
           <h2>🏆 Welcome to the application. 🏆</h2>
         </div>
         {/* Security bad bractice | for **Demo App Only** */}
         {/* Using Images from third party URLs is not recommended */}
         {/* Better use trusted CDNs. but for now its peanut butter jelly time */}
         <img
+          style={{width: pbjSize[0]*4}}
           src="https://www.enworld.org/media/peanut-butter-jelly-time-banana-gif.95112/full"
           alt="peanut-butter-jelly-time.gif"
         />
         <br />
         <br />
-        <button
+
+        <Slider value={pbjSize} min={0} max={100} defaultValue={[50]} step={1} onValueChange={onSliderChange}/>
+
+        <br />
+
+        <Button
           className="w-full rounded bg-red-600 text-white uppercase font-bold border-none h-10 hover:bg-red-700"
           onClick={onClick}
         >
           Sign Out
-        </button>
+        </Button>
+
       </div>
     </div>
   );
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,2 +1,3 @@
 export * as constants from './constants';
 export * as envs from './envs';
+export * as misc from './misc';
